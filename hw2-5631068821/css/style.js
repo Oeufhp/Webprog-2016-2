4679a6fbfa0ae6e24652d87e210362049b44d9db
Pitarn Harnpanichpun <pitarnh@outlook.com>
2017-02-09 21:32:59 +0700

    finish first ver

--- hw2-5631068821/css/style.js
+++ hw2-5631068821/css/style.js
@@ -3,25 +3,22 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   '#header-container': {
     'marginTop': [{ 'unit': 'px', 'value': 20 }],
-    'width': [{ 'unit': '%H', 'value': 0.83 }],
     'backgroundColor': '#fff',
-    'marginLeft': [{ 'unit': 'string', 'value': 'auto' }],
-    'marginRight': [{ 'unit': 'string', 'value': 'auto' }],
     'borderTopLeftRadius': '6px',
     'borderTopRightRadius': '6px'
   },
   '#web-logo': {
     'width': [{ 'unit': '%H', 'value': 0.4 }],
-    'margin': [{ 'unit': 'px', 'value': 30 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }]
-  },
-  '#web-quote': {
-    'paddingBottom': [{ 'unit': 'px', 'value': 10 }]
-  },
-  'w3-input': {
-    'display': 'inline'
+    'screen&&<w420': {
+      'width': [{ 'unit': '%H', 'value': 1 }]
+    },
+    'screen&&>w421&&<w768': {
+      'width': [{ 'unit': '%H', 'value': 0.7 }],
+      'marginTop': [{ 'unit': 'px', 'value': 20 }]
+    }
   },
   '#myNavbar': {
-    'width': [{ 'unit': '%H', 'value': 0.85 }],
+    'width': [{ 'unit': '%H', 'value': 1 }],
     'marginLeft': [{ 'unit': 'string', 'value': 'auto' }],
     'marginRight': [{ 'unit': 'string', 'value': 'auto' }]
   },
@@ -39,38 +36,51 @@ export default StyleSheet.create({
     'backgroundColor': '#fff',
     'marginLeft': [{ 'unit': 'string', 'value': 'auto' }],
     'marginRight': [{ 'unit': 'string', 'value': 'auto' }],
-    'width': [{ 'unit': '%H', 'value': 0.83 }]
+    'width': [{ 'unit': '%H', 'value': 1 }]
+  },
+  'w3-card-2': {
+    'marginBottom': [{ 'unit': 'px', 'value': 10 }],
+    'screen&&>w1023&&<w1360': {
+      'marginRight': [{ 'unit': 'px', 'value': -5 }]
+    }
+  },
+  'w3-card-2>p': {
+    'textAlign': 'center',
+    'screen&&<w420': {
+      'fontSize': [{ 'unit': 'px', 'value': 10 }]
+    }
+  },
+  'content-2card': {
+    'width': [{ 'unit': '%H', 'value': 1 }]
+  },
+  'content-3card': {
+    'width': [{ 'unit': '%H', 'value': 1 }]
   },
   'content-3card>div': {
-    'width': [{ 'unit': '%H', 'value': 0.55 }],
-    'fontSize': [{ 'unit': 'px', 'value': 14 }],
-    'marginRight': [{ 'unit': 'px', 'value': 20 }],
-    'backgroundColor': '#fff'
+    'fontSize': [{ 'unit': 'px', 'value': 13 }],
+    'backgroundColor': '#fff',
+    'marginRight': [{ 'unit': 'px', 'value': 5 }]
   },
   'web-content-img': {
     'width': [{ 'unit': '%H', 'value': 1 }]
   },
   'w3-btn': {
     'borderRadius': '2px',
-    'margin': [{ 'unit': 'px', 'value': 15 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 7 }],
-    'backgroundColor': '#1a9085',
-    // box-shadow: 0 5px 7px 0 rgba(0,0,0,0.20),0 8px 25px 0 rgba(0,0,0,0.10);
+    'margin': [{ 'unit': 'px', 'value': 15 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }],
+    'backgroundColor': '#1a9085'
   },
   'w3-btn:hover': {
     'backgroundColor': '#fff',
     'color': '#1a9085',
     'border': [{ 'unit': 'string', 'value': 'solid' }, { 'unit': 'px', 'value': 1 }, { 'unit': 'string', 'value': '#1a9085' }]
   },
-  'content-2card': {
-    'display': 'flex'
-  },
-  'content-3card': {
-    'display': 'flex'
-  },
   'content-2card>div': {
-    'width': [{ 'unit': '%H', 'value': 0.3 }],
-    'fontSize': [{ 'unit': 'px', 'value': 14 }],
-    'marginRight': [{ 'unit': 'px', 'value': 30 }],
+    'fontSize': [{ 'unit': 'px', 'value': 13 }],
+    'marginRight': [{ 'unit': 'px', 'value': 15 }],
     'backgroundColor': '#fff'
+  },
+  'iframe': {
+    'width': [{ 'unit': '%H', 'value': 1 }],
+    'height': [{ 'unit': 'px', 'value': 300 }]
   }
 });
